5ba0b8e12374bf643be2a45bd5b135c580dedec1
webdesign4alll <190048@tkmce.ac.in>
2021-10-20 23:04:31 +0530

    added basic structure of homepage not responsive #1

---
+++ muiz/script.js
@@ -0,0 +1,17 @@
+https://www.electronicshub.org/8051-microcontroller-pin-diagram/
+
+1.RC Coupled Amplifier
+https://drive.google.com/file/d/1pDiGsOFDCtpJx8yZ0g5aYw1UG42NvI4t/view?usp=drivesdk
+
+2. Rc coupled amplifier design
+https://drive.google.com/file/d/1BIQ_QSRPN7MR-Qa-ptAYUAatWZtYCPhl/view?usp=drivesdk
+
+3. Rc coupled amplifier design: Anzar sir
+https://drive.google.com/file/d/1BzrHGrUcSzFX5nTTo4ebsxAEueEJ2c-v/view?usp=drivesdk
+
+4. Rc amplifier simulation, frequency response
+https://drive.google.com/file/d/13BbE3AL2IU0H6jkJROQDRYry21jZTI4q/view?usp=drivesdk
+
+
+
+8051 microcontroller by mazidi lab links http://www.microdigitaled.com/8051/Lab/8051_labs.htm
